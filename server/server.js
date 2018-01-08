5807e80898a358c6ed8591d52c008e42259e47e1
Donald <donald@shoppingminds.com>
2018-01-08 16:02:20 +0100

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,14 +14,30 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New user connected');
 
+	socket.emit('newMessage', {
+		from: 'Admin',
+		text: 'Wlecome to the caht-app',
+		createdAt: new Date().getTime()
+	});
+
+	socket.broadcast.emit('newMessage', {
+		from: 'Admin',
+		text: 'New user joined chat-app',
+		createdAt: new Date().getTime()
+	});
+
 	socket.on('createMessage', (message) => {
 		console.log('Created message', message);
 		io.emit('newMessage', {
-		// socket.broadcast.emit('newMessage', {
 			from: message.from,
 			text: message.text,
 			createdAt: new Date().getTime()
 		});
+		// socket.broadcast.emit('newMessage', {
+		// 	from: message.from,
+		// 	text: message.text,
+		// 	createdAt: new Date().getTime()
+		// });
 	});
 
 	socket.on('disconnect', () => {
