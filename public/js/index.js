6e32367b353dc529d55ce53a45b73112e16cb6d1
Donald <donald@shoppingminds.com>
2018-01-09 15:21:18 +0100

    Add scroll to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,20 @@ socket.on('connect', function() {
 	console.log('Connected to server');
 });
 
+function scrollToBottom() {
+	var messages = jQuery('#messages');
+	var newMessage = messages.children('li:last-child');
+	var clientHeight = messages.prop('clientHeight');
+	var scrollTop = messages.prop('scrollTop');
+	var scrollHeight = messages.prop('scrollHeight');
+	var newMessageHeight = newMessage.innerHeight();
+	var lastMessageHeight = newMessage.prev().innerHeight();
+
+	if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+		messages.scrollTop(scrollHeight);
+	}
+}
+
 socket.on('newMessage', function(message) {
 	var formattedTime = moment(message.createdAt).format('h:mm a');
 	var template = jQuery('#message-template').html();
@@ -14,6 +28,7 @@ socket.on('newMessage', function(message) {
 	});
 
 	jQuery('#messages').append(html);
+	scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message) {
@@ -26,6 +41,7 @@ socket.on('newLocationMessage', function(message) {
 	});
 
 	jQuery('#messages').append(html);
+	scrollToBottom();
 });
 
 socket.on('disconnect', function() {
